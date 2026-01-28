1d4e6e1fe023e56065c3802507ab82cb5c21047f
Sarthak-2329 <sarthaksurale23@gmail.com>
2026-01-29 00:34:09 +0530

    send welcome email

--- chat-application/backend/src/server.js
+++ chat-application/backend/src/server.js
@@ -1,16 +1,15 @@
 import express from 'express';
-import dotenv from 'dotenv';
 import path from 'path';
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from './lib/db.js';
-dotenv.config();
+import { ENV } from './lib/env.js';
 
 const app = express();
 const __dirname = path.resolve();
 
-const PORT = process.env.PORT;
+const PORT = ENV.PORT;
 
 app.use(express.json())
 
@@ -18,7 +17,7 @@ app.use("/api/auth",authRoutes);
 app.use("/api/message",messageRoutes);
 
 // deployment purposes
-if(process.env.NODE_ENV==="production"){
+if(ENV.NODE_ENV==="production"){
     app.use(express.static(path.join(__dirname,"../frontend/dist")));
 
     app.get("*",(req,res)=>{
