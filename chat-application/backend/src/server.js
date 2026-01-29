00cd7b762a900198e9ac7935d86cb4162a943b2c
Sarthak-2329 <sarthaksurale23@gmail.com>
2026-01-29 20:26:25 +0530

    update profile and profile pic

--- chat-application/backend/src/server.js
+++ chat-application/backend/src/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import path from 'path';
-
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from './lib/db.js';
@@ -11,7 +11,8 @@ const __dirname = path.resolve();
 
 const PORT = ENV.PORT;
 
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
 app.use("/api/message",messageRoutes);
