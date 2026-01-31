f95b505a8da3a11b7c0affc30bf126c551ec77d8
Sarthak-2329 <sarthaksurale23@gmail.com>
2026-01-31 23:57:22 +0530

    signup-frontend

--- chat-application/frontend/src/store/useAuthStore.js
+++ chat-application/frontend/src/store/useAuthStore.js
@@ -1,10 +1,34 @@
 import {create} from 'zustand';
+import {axiosInstance} from "../lib/axios";
+import toast from 'react-hot-toast';
 
 export const useAuthStore = create((set)=>({
-    authUser: {name:"sarthak", _id:2329,age:21},
-    isLoggedIn:false,
-    login: ()=>{
-        console.log("I just logged in");
-        set({isLoggedIn:true});
+    authUser:null,
+    isCheckingAuth:true,
+    isSigningUp:false,
+
+    checkAuth: async ()=>{
+        try {
+            const res = await axiosInstance.get("/auth/check");
+            set({authUser:res.data});
+        } catch (error) {
+            console.log("Error in authCheck: ",error);
+            set({authUser:null});
+        } finally{
+            set({isCheckingAuth:false});
+        }
+    },
+
+    signup: async (data)=>{
+        set({isSigningUp:true});
+        try {
+            const res = await axiosInstance.post("/auth/signup",data);
+            set({authUser:res.data});
+            toast.success("Account created successfully!");
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }finally{
+            set({isSigningUp:false});
+        }
     },
 }));
