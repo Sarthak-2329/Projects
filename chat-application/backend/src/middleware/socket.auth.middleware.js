197ae2a161e055fc50000672e8a19419484203b5
Sarthak-2329 <sarthaksurale23@gmail.com>
2026-02-04 00:07:16 +0530

    online users

---
+++ chat-application/backend/src/middleware/socket.auth.middleware.js
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/User.js';
+import {ENV} from '../lib/env.js';
+
+export const socketAuthMiddleware = async (socket, next) => {
+    try {
+        const token = socket.handshake.headers.cookie ?.split("; ").find((row)=>row.startsWith("jwt="))?.split("=")[1];
+        if (!token) {
+            console.log("No token provided");
+            return next(new Error("Authentication error: Token not provided"));
+        }
+        const decoded = jwt.verify(token, ENV.JWT_SECRET);
+        if(!decoded){
+            console.log("Invalid token");
+            return next(new Error("Authentication error: Invalid token"));
+        }
+        const user = await User.findById(decoded.userId).select("-password");
+        if(!user){
+            console.log("User not found");
+            return next(new Error("Authentication error: User not found"));
+        }
+
+        socket.user = user;
+        socket.userId = user._id.toString();
+        console.log(`User ${user.fullname} ${user._id} authenticated via socket`);
+        next();
+    } catch (error) {
+        console.log("Authentication error:", error.message);
+        next(new Error("Authentication error"));
+    }
+};
